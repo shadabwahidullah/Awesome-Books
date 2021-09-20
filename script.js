17cb32e78a9cfa54a7742e45add227eb111d5605
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-09-21 00:30:13 +0100

    Modify removeBook function

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const author = document.getElementById('author');
 const addBtn = document.querySelector('.btnAdd');
 const list = document.querySelector('.booklist');
 
-const books = JSON.parse(localStorage.getItem('books'))||[];
-
-
-
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
 // function to add books
 function addBook(title, author) {
@@ -15,7 +12,17 @@ function addBook(title, author) {
   return { title, author };
 }
 
-function createBook({title, author}) {
+// function to remove book
+function removeBook(element) {
+  if (element.classList.contains('remove')) {
+    const removeItem = element.parentElement;
+    books.splice(removeItem, 1);
+    removeItem.remove();
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+function createBook({ title, author }) {
   const divWrapper = document.createElement('div');
   const titleHeader = document.createElement('h3');
   const authorHeader = document.createElement('h3');
@@ -29,28 +36,14 @@ function createBook({title, author}) {
 
   divWrapper.append(titleHeader, authorHeader, removebtn, hrLine);
   list.appendChild(divWrapper);
-  
-  removebtn.addEventListener('click',(e) => {
+
+  removebtn.addEventListener('click', (e) => {
     const removeItem = e.target;
     removeBook(removeItem);
   });
-
 }
 
 books.forEach(createBook);
-
-// function to remove book
-function removeBook(index) {
-  if(index.classList.contains('remove')){
-    let temp = index.parentElement;
-    books.splice(temp, 1);
-    temp.remove();
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
-
-
-
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -60,6 +53,3 @@ addBtn.addEventListener('click', (e) => {
   author.value = '';
   title.focus();
 });
-
-
-
