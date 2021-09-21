def11ac6d164daf19e3c32a9cf0bab3fecb3e0f5
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-09-21 22:44:49 +0100

    Correct all the suggested

--- bookList.js
+++ bookList.js
@@ -3,14 +3,12 @@ class Booklist {// eslint-disable-line no-unused-vars
     this.books = JSON.parse(localStorage.getItem('books')) || [];
   }
 
-  // function to add books
   addBook(title, author, id) {
-    this.books.push({ title, author, id });
+    this.books.concat({ title, author, id });
     localStorage.setItem('books', JSON.stringify(this.books));
     return { title, author, id };
   }
 
-  // function to remove book
   removeBook(element, id) {
     if (element.classList.contains('remove')) {
       const removeItem = element.parentElement;
