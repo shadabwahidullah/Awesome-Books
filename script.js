596e2601118f18989d1b432637d6485139e88373
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-21 20:07:53 +0430

    Resolve remove button's functionality

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const addBtn = document.querySelector('.btnAdd');
-const list = document.querySelector('.booklist');
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const addBtn = document.querySelector(".btnAdd");
+let list = document.querySelector(".booklist");
 
-const books = JSON.parse(localStorage.getItem('books')) || [];
+let books = JSON.parse(localStorage.getItem("books")) || [];
 
 // function to add books
-function addBook(title, author) {
-  books.push({ title, author });
-  localStorage.setItem('books', JSON.stringify(books));
-  return { title, author };
+function addBook(title, author, date) {
+  books.push({ title, author, date });
+  localStorage.setItem("books", JSON.stringify(books));
+  return { title, author, date };
 }
 
+let modifiedBooks;
 // function to remove book
 function removeBook(element) {
-  if (element.classList.contains('remove')) {
+  if (element.classList.contains("remove")) {
     const removeItem = element.parentElement;
-    books.splice(removeItem, 1);
+
+    books = books.filter((book) => {
+      if (book.date != element.id) {
+        return true;
+      }
+      return false;
+    });
     removeItem.remove();
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
 }
 
-function createBook({ title, author }) {
-  const divWrapper = document.createElement('div');
-  const titleHeader = document.createElement('h3');
-  const authorHeader = document.createElement('h3');
-  const removebtn = document.createElement('button');
-  const hrLine = document.createElement('hr');
+function createBook({ title, author, date }) {
+  const divWrapper = document.createElement("div");
+  const titleHeader = document.createElement("h3");
+  const authorHeader = document.createElement("h3");
+  const removebtn = document.createElement("button");
+  removebtn.id = date;
+  const hrLine = document.createElement("hr");
 
   titleHeader.innerText = title;
   authorHeader.innerText = author;
-  removebtn.textContent = 'Remove';
-  removebtn.classList.add('remove');
+  removebtn.textContent = "Remove";
+  removebtn.classList.add("remove");
 
   divWrapper.append(titleHeader, authorHeader, removebtn, hrLine);
   list.appendChild(divWrapper);
 
-  removebtn.addEventListener('click', (e) => {
+  removebtn.addEventListener("click", (e) => {
     const removeItem = e.target;
     removeBook(removeItem);
   });
@@ -45,17 +53,21 @@ function createBook({ title, author }) {
 
 books.forEach(createBook);
 
-addBtn.addEventListener('click', (e) => {
+addBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (title.validity.valueMissing && author.validity.valueMissing) {
-    title.setCustomValidity('Please enter title!');
-    author.setCustomValidity('Please enter author\'s name');
+    title.setCustomValidity("Please enter title!");
+    author.setCustomValidity("Please enter author's name");
   } else {
-    const newBook = addBook(title.value, author.value);
+    const newBook = addBook(
+      title.value,
+      author.value,
+      new Date().getTime().toString()
+    );
     createBook(newBook);
-    title.value = '';
-    author.value = '';
+    title.value = "";
+    author.value = "";
     title.focus();
   }
 });
